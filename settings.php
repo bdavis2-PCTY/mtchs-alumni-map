788015396f2740b98e8bc3898fcf9b7e97c38c57
Braydon Davis <braydon.davis@mtchs.org>
2017-05-04 09:14:19 -0600

    Settings update

--- settings.php
+++ settings.php
@@ -2,5 +2,5 @@
 
 // SETTINGS FOR MTCHS ALUMNI MAP
 
-$SMTP_OVERRIDE_EMAIL = "braydon.davis@mtchs.org";		// Email to alert for new alumni requests 
-$APP_PATH = "http://braydond.smtchs.org/alumni";		// URL to the main alumni map page without a tailing '/'
+$SMTP_OVERRIDE_EMAIL = "fakeemailaddress@mtchs.org"; 	// Email to alert for new alumni requests 
+$APP_PATH = "http://mtchs.org/alumni";					// URL to the main alumni map page without a tailing '/'
